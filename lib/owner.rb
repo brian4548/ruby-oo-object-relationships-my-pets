4f24d9c0bfe9bdad8fb13bcdd55b03ff1da0efa5
brian4548 <brianr4548@gmail.com>
2020-11-19 23:45:33 -0600

    commit tree 0e7134e7a9f74a8ca8a04ddb2ea7d1e58015f8da

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,32 @@
 class Owner
-  # code goes here
-end
+  
+  attr_accessor 
+  attr_reader :name, :species
+
+@@all = []
+@@count = []
+@@reset_all = []
+  
+  def initialize (name)
+    @name = name
+    @species = "human"
+    @@all << self
+    @@count << self
+  end
+
+
+
+  def say_species
+    @species = "I am a human."
+  end 
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@count
+  end 
+
+
+end 
